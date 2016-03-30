fc8e1288d6c032c5e521c4be345295bb21f3b236
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-03-30 21:47:32 +0100

    Added RESTful webservice to websocket_broadcast

--- websocket_broadcast.js
+++ websocket_broadcast.js
@@ -1,15 +1,23 @@
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: 9090 });
+var express = require('express');
+var app = express();
 
-wss.on('connection', function connection(ws) {
-
-  // Create event listener
-  ws.on('message', function(message) {
-
-    wss.clients.forEach(function each(client) {
-      client.send("broadcast:" + message);
-    });
-
-  });
+// Start REST server on port 8081
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Websocket event broadcaster REST API listening on http://%s:%s", host, port)
 });
+
+
+app.get('/events', function (req, res) {
+   console.log('Event');
+   wss.clients.forEach(function each(client) {
+      client.send("broadcast:" + "TODO");
+    });
+   res.sendStatus(200);
+});
+
+
 
